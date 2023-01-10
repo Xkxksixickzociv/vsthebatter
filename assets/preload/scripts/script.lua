308e34c1345d62638c771363aec8b0717bea6d34
Xkxksixickzociv <119426594+Xkxksixickzociv@users.noreply.github.com>
2023-01-10 21:10:04 +0200

    Add files via upload

---
+++ assets/preload/scripts/script.lua
@@ -0,0 +1,5 @@
+function onCreate() 
+makeLuaText('watermark',"Ported By NickYT FNF ", 0, 2, 688); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 35);
+    addLuaText('watermark');
+end
